0641aaf4b175a0308ed2c6a5923978846bddb46a
Trey R <84750467+trusell09@users.noreply.github.com>
2022-02-15 18:37:17 -0600

    Update ArraysListPrac.java

--- ArraysListPrac/ArraysListPrac.java
+++ ArraysListPrac/ArraysListPrac.java
@@ -7,20 +7,35 @@ public class ArraysListPrac {
         System.out.println("Hello, Arrays Lists!");
 
         // initialize an arraylist with values
-        ArrayList <String> names = new ArrayList<String>(Arrays.asList("Mark", "Willie"));
+        //ArrayList <String> names = new ArrayList<String>(Arrays.asList("Mark", "Willie"));
         
         
         // initialize an empty arraylist with no values BELOW
-        //ArrayList <String> names = new ArrayList<String>();
-        //names.add("Murph");
-        //names.add("Henryson");
+        ArrayList <String> names = new ArrayList<String>();
+        names.add("Murph");
+        names.add("Henryson");
         // names.remove(0);  would remove Murph from the list of names when printed 
         // names.set(0, "New Name");  Sets the new value as "New Name" or whatever value given
-        
+
+
         System.out.println(names);
         System.out.println(names.get(0));
         
+        
+        // looping through the length of Arraylist 
+        // below will Loop through Murph and Henryson names
+        for (int i = 0; i<names.size(); i++){
+            System.out.println("Looping . I is -->" + i);
+            System.out.println(names.get(i)); //same saying [i] in JavaScript or Python
+        }
     
-    
+        //enhanced for loop is when we dont need the index numbers, just the values at the index numbers
+        //dont use enhanced for loop when you need to know the index numbers
+        for(String name : names){
+            System.out.println(name);
+        }
+
+
+
     }
 }
